ca9b1df2c0497b18526a759bd3898a6a76a83ca6
문종훈 <lune6669@ant-soft.co.kr>
2020-12-14 11:20:13 +0900

    init

---
+++ src/main/java/com/moon/MvpApplication.java
@@ -0,0 +1,13 @@
+package com.moon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.moon"})
+public class MvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpApplication.class, args);
+	}
+
+}
